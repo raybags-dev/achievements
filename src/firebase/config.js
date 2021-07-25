04e8163f1f70ee215af91c0d9aded5c1299634c2
raybags <baguma.github@gmail.com>
2021-07-25 16:20:42 +0200

    intergrated firebase auth, database and

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,7 +15,7 @@ const {
 var firebaseConfig = {
   apiKey: REACT_APP_PHOTO_GALLERY_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: "photo-gallery-e0663",
+  projectId: "achievements-collection",
   storageBucket: REACT_APP_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_PHOTO_MESSAGING_SENDER_ID,
   appId: REACT_APP_APP_ID,
